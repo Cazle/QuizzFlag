6ce2f887237e39e4d5dbc23ab54c382cb15161fc
Cazle <kykyglt@gmail.com>
2024-02-06 11:05:15 +0100

    first commit

---
+++ QuizzFlag/Model/Countries.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+protocol Country {
+    
+    var name: String { get set }
+    var flag: String { get set }
+    var flagHistory: String { get set }
+    var coatOfArms: String { get set }
+    var dateOfFlagCreation: Int { get set }
+    
+}
---
+++ QuizzFlag/Model/MainScreen.swift
@@ -0,0 +1,8 @@
+//
+//  MainScreen.swift
+//  QuizzFlag
+//
+//  Created by Kyllian GUILLOT on 05/02/2024.
+//
+
+import Foundation
